37a95684a6e67411a971e728c151a836f4a2dc9e
Ahmed Mansour <37387373+AhmedMansour9@users.noreply.github.com>
2018-09-08 02:24:07 +0200

    ")"

--- app/src/main/java/ikon/ikon/Adapter/Phones_Adapter.java
+++ app/src/main/java/ikon/ikon/Adapter/Phones_Adapter.java
@@ -1,13 +1,26 @@
 package ikon.ikon.Adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import ikon.ikon.Fragments.Phones;
+import ikon.ikon.Model.Phone;
+import ikon.ikon.Model.Phones;
+import ikon.ikon.R;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by ic on 9/7/2018.
@@ -16,91 +29,71 @@ import ikon.ikon.Fragments.Phones;
 public class Phones_Adapter extends RecyclerView.Adapter<Phones_Adapter.MyViewHolder> {
 
 
-    //    private ArrayList<Friendsetandget> mArrayList=new ArrayList<>();
-    Context context;
-    int  lastPosition=-1;
-    public Boolean bollean;
 
     public List<Phones> filteredList=new ArrayList<>();
     View itemView;
+    Context con;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public TextView title;
-
-
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView T_Name,T_Discrption,T_Model,T_Price;
+        ImageView mobile;
+        ProgressBar progressBar;
         public MyViewHolder(View view) {
             super(view);
-            title =  view.findViewById(R.id.textfriendd);
-            photoo=view.findViewById(R.id.imgfriend);
-           );           viw.setOnClickListener(this);
+            T_Name = view.findViewById(R.id.T_Name);
+            T_Discrption = view.findViewById(R.id.T_Discrption);
+            T_Model = view.findViewById(R.id.T_Model);
+            T_Price = view.findViewById(R.id.T_Price);
+            mobile=view.findViewById(R.id.Image_Phone);
+          progressBar=view.findViewById(R.id.progrossimage);
 
 
         }
-        @Override
-        public void onClick(View view) {
-            if (clickListene != null) clickListene.onClick(view, getLayoutPosition());
-        }
+
     }
 
-   c void setClickListener(ItemClickListener itemClickListener) {
-   his.clickListene = itemClickListener;
-
-   c void setClickButton(btnclickinterface btnclic){
-   his.btnclick=btnclic;
-
-   c MoviesAdapter(ArrayList<Friendsetandget> moviesList,Context context ) {
-   ilteredList=moviesList;
-   his.context=context;
+    public Phones_Adapter(List<Phones> phon,Context context){
+        filteredList=phon;
+        this.con=context;
+    }
 
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.itsmrecyclefriends, parent, false);
+                .inflate(R.layout.item_phone, parent, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, final int position) {
-        Friendsetandget movie = filteredList.get(position);
-        FragmentFriends.check=false;
 
-        if (movie.getUsername() != null) {
-            holder.title.setText(movie.getUsername());
-        } else if (movie.getUsername() == null) {
-            holder.title.setText(movie.GetEmail());
-        }
 
-        String i = movie.getPhoto();
+        holder.T_Name.setText(filteredList.get(position).getProductsName());
+        holder.T_Model.setText(filteredList.get(position).getProductsModel());
+        holder.T_Discrption.setText(filteredList.get(position).getProductsDescription());
+        holder.T_Price.setText(filteredList.get(position).getProductsPrice());
+        String i = filteredList.get(position).getProductsImage();
+       
         Uri u = Uri.parse(i);
-
+       holder.progressBar.setVisibility(View.VISIBLE);
         Picasso.with(getApplicationContext())
-                .load(u)
-                .placeholder(R.drawable.emptyprofile)
-                .into(holder.photoo);
+                .load("http://ikongo.com/site/"+u)
+                .into(holder.mobile, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        holder.progressBar.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onError() {
+                        holder.progressBar.setVisibility(View.GONE);
+                    }
+                });
 
 
-        holder.btndelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnclick.onClickCallback(view,holder.getAdapterPosition());
-            }
-        });
 
 
-
-        bollean=movie.getOnline();
-        if(bollean==true){
-            holder.onlinee.setVisibility(View.VISIBLE);
-        }else {
-            holder.onlinee.setVisibility(View.INVISIBLE);
-        }
-
-        Animation animation = AnimationUtils.loadAnimation(context,
-                (position > lastPosition) ? R.anim.recycle_up_from_bottom
-                        : R.anim.recycle_down_from_top);
-        holder.itemView.startAnimation(animation);
-        lastPosition = position;
 
 
 
--- app/src/main/java/ikon/ikon/Fragments/Phones.java
+++ app/src/main/java/ikon/ikon/Fragments/Phones.java
@@ -4,12 +4,18 @@ package ikon.ikon.Fragments;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.List;
 
+import ikon.ikon.Adapter.Phones_Adapter;
 import ikon.ikon.Model.phonesResponse;
 import ikon.ikon.PreSenter.GetPhonesPresenter;
 import ikon.ikon.R;
@@ -21,7 +27,7 @@ import static android.content.Context.MODE_PRIVATE;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Phones extends Fragment implements PhonesView{
+public class Phones extends Fragment implements PhonesView,SwipeRefreshLayout.OnRefreshListener{
 
 
     public Phones() {
@@ -29,7 +35,11 @@ public class Phones extends Fragment implements PhonesView{
     }
      GetPhonesPresenter phons;
      View view;
+     RecyclerView recyclerView;
      SharedPreferences shared;
+     Phones_Adapter adapter;
+     String Lan;
+     SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -37,25 +47,74 @@ public class Phones extends Fragment implements PhonesView{
         view=inflater.inflate(R.layout.fragment_phones, container, false);
        phons=new GetPhonesPresenter(getContext(),this);
         shared=getActivity().getSharedPreferences("Language",MODE_PRIVATE);
-        String Lan=shared.getString("Lann",null);
+         Lan=shared.getString("Lann",null);
+         Recyclview();
+        SwipRefresh();
         if(Lan!=null) {
             phons.GetPhones(Lan);
         }else {
             phons.GetPhones("en");
         }
-
-
+          mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+              @Override
+              public void onRefresh() {
+              phons.GetPhones("ar");
+              }
+          });
 
         return view;
     }
 
     @Override
     public void getPhones(List<ikon.ikon.Model.Phones> phone) {
+        Toast.makeText(getContext(), ""+phone.size(), Toast.LENGTH_SHORT).show();
+        adapter = new Phones_Adapter(phone,getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
+        mSwipeRefreshLayout.setEnabled(false);
     }
 
     @Override
     public void Errorphones() {
 
+    }
+    public void Recyclview(){
+        recyclerView = view.findViewById(R.id.recycler_phones);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    public void SwipRefresh(){
+        mSwipeRefreshLayout =  view.findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_blue_dark);
+
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                if(Lan!=null) {
+                    phons.GetPhones(Lan);
+                }else {
+                    phons.GetPhones("en");
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onRefresh() {
+        if(Lan!=null) {
+            phons.GetPhones(Lan);
+        }else {
+            phons.GetPhones("en");
+        }
     }
 }
